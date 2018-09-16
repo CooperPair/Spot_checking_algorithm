ba1d95aaa17c70d43c865260ac129662c793cbd4
ranand5 <hitanand4@gmail.com>
2018-09-16 19:53:29 +0530

    adding the optimized code for regression problem

---
+++ regression.py
@@ -0,0 +1,121 @@
+import warnings
+from numpy import mean
+from numpy import std
+from sklearn.datasets import make_regression
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import HuberRegressor
+from sklearn.linear_model import Lars
+from sklearn.linear_model import LassoLars
+from sklearn.linear_model import PassiveAggressiveRegressor
+from sklearn.linear_model import RANSACRegressor
+from sklearn.linear_model import SGDRegressor
+from sklearn.linear_model import TheilSenRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import ExtraTreeRegressor
+from sklearn.svm import SVR
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.ensemble import BaggingRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+import crayons 
+
+# create a dict of standard models to evaluate {name:object}
+def get_models(models=dict()):
+	# linear models
+	models['lr'] = LinearRegression()
+	alpha = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+	for a in alpha:
+		models['lasso-'+str(a)] = Lasso(alpha=a)
+	for a in alpha:
+		models['ridge-'+str(a)] = Ridge(alpha=a)
+	for a1 in alpha:
+		for a2 in alpha:
+			name = 'en-' + str(a1) + '-' + str(a2)
+			models[name] = ElasticNet(a1, a2)
+	models['huber'] = HuberRegressor()
+	models['lars'] = Lars()
+	models['llars'] = LassoLars()
+	models['pa'] = PassiveAggressiveRegressor(max_iter=1000, tol=1e-3)
+	models['ranscac'] = RANSACRegressor()
+	models['sgd'] = SGDRegressor(max_iter=1000, tol=1e-3)
+	models['theil'] = TheilSenRegressor()
+	# non-linear models
+	n_neighbors = range(1, 21)
+	for k in n_neighbors:
+		models['knn-'+str(k)] = KNeighborsRegressor(n_neighbors=k)
+	models['cart'] = DecisionTreeRegressor()
+	models['extra'] = ExtraTreeRegressor()
+	models['svml'] = SVR(kernel='linear')
+	models['svmp'] = SVR(kernel='poly')
+	c_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+	for c in c_values:
+		models['svmr'+str(c)] = SVR(C=c)
+	# ensemble models
+	n_trees = 100
+	models['ada'] = AdaBoostRegressor(n_estimators=n_trees)
+	models['bag'] = BaggingRegressor(n_estimators=n_trees)
+	models['rf'] = RandomForestRegressor(n_estimators=n_trees)
+	models['et'] = ExtraTreesRegressor(n_estimators=n_trees)
+	models['gbm'] = GradientBoostingRegressor(n_estimators=n_trees)
+	print('Defined %d models' % len(models))
+	return models
+
+#evaluate a dict of models {name:object}, returns {name:score}
+def evaluate_models(X, y, models, folds=10, metric='accuracy'):
+	results = dict()
+	for name, model in models.items():
+		# evaluate the model
+		scores = robust_evaluate_model(X, y, model, folds, metric)
+		# show process
+		if scores is not None:
+			# store a result
+			results[name] = scores
+			mean_score, std_score = mean(scores), std(scores)
+			print(crayons.blue(f'\t[*] NAME => {name}', bold=True))
+			print(crayons.yellow(f'\t[*] Mean Score => {round(mean_score,3)}', bold = True))
+			print(crayons.red(f'\t[*] Std_Score => (+/-){round(std_score,3)}', bold=True))
+			print("\n")
+		else:
+			print('>%s: error' % name)
+	return results
+
+# create a feature preparation pipeline for a model
+def make_pipeline(model):
+	steps = list()
+	# standardization
+	steps.append(('standardize', StandardScaler()))
+	# normalization
+	steps.append(('normalize', MinMaxScaler()))
+	# the model
+	steps.append(('model', model))
+	# create pipeline
+	pipeline = Pipeline(steps=steps)
+	return pipeline
+ 
+# evaluate a single model
+def evaluate_model(X, y, model, folds, metric):
+	# create the pipeline
+	pipeline = make_pipeline(model)
+	# evaluate model
+	scores = cross_val_score(pipeline, X, y, scoring=metric, cv=folds, n_jobs=-1)
+	return scores
+ 
+# evaluate a model and try to trap errors and hide warnings
+def robust_evaluate_model(X, y, model, folds, metric):
+	scores = None
+	try:
+		with warnings.catch_warnings():
+			warnings.filterwarnings("ignore")
+			scores = evaluate_model(X, y, model, folds, metric)
+	except:
+		scores = None
+	return scores
--- spot_checking_regression.py
+++ spot_checking_regression.py
@@ -5,38 +5,15 @@ import numpy as np
 from numpy import mean
 from numpy import std
 from matplotlib import pyplot
-from sklearn.datasets import make_regression
-from sklearn.model_selection import cross_val_score
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import Lasso
-from sklearn.linear_model import Ridge
-from sklearn.linear_model import ElasticNet
-from sklearn.linear_model import HuberRegressor
-from sklearn.linear_model import Lars
-from sklearn.linear_model import LassoLars
-from sklearn.linear_model import PassiveAggressiveRegressor
-from sklearn.linear_model import RANSACRegressor
-from sklearn.linear_model import SGDRegressor
-from sklearn.linear_model import TheilSenRegressor
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.tree import ExtraTreeRegressor
-from sklearn.svm import SVR
-from sklearn.ensemble import AdaBoostRegressor
-from sklearn.ensemble import BaggingRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import ExtraTreesRegressor
-from sklearn.ensemble import GradientBoostingRegressor
 import sys
 import crayons
+import regression
 
+'''
 # load the dataset, returns X and y elements
 def load_dataset():
 	return make_regression(n_samples=1000, n_features=50, noise=0.1, random_state=1)
-
+'''
 data = pd.read_csv(sys.argv[1])
 #dropping unnecessary columns..
 data = data.drop(['Symbol','Series','Date','% Dly Qt to Traded Qty'],axis = 1)
@@ -45,98 +22,8 @@ m = np.array(data['Close Price'])
 data = data.drop('Close Price',axis=1)
 labels = np.array(data)
 
-# create a dict of standard models to evaluate {name:object}
-def get_models(models=dict()):
-	# linear models
-	models['lr'] = LinearRegression()
-	alpha = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-	for a in alpha:
-		models['lasso-'+str(a)] = Lasso(alpha=a)
-	for a in alpha:
-		models['ridge-'+str(a)] = Ridge(alpha=a)
-	for a1 in alpha:
-		for a2 in alpha:
-			name = 'en-' + str(a1) + '-' + str(a2)
-			models[name] = ElasticNet(a1, a2)
-	models['huber'] = HuberRegressor()
-	models['lars'] = Lars()
-	models['llars'] = LassoLars()
-	models['pa'] = PassiveAggressiveRegressor(max_iter=1000, tol=1e-3)
-	models['ranscac'] = RANSACRegressor()
-	models['sgd'] = SGDRegressor(max_iter=1000, tol=1e-3)
-	models['theil'] = TheilSenRegressor()
-	# non-linear models
-	n_neighbors = range(1, 21)
-	for k in n_neighbors:
-		models['knn-'+str(k)] = KNeighborsRegressor(n_neighbors=k)
-	models['cart'] = DecisionTreeRegressor()
-	models['extra'] = ExtraTreeRegressor()
-	models['svml'] = SVR(kernel='linear')
-	models['svmp'] = SVR(kernel='poly')
-	c_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-	for c in c_values:
-		models['svmr'+str(c)] = SVR(C=c)
-	# ensemble models
-	n_trees = 100
-	models['ada'] = AdaBoostRegressor(n_estimators=n_trees)
-	models['bag'] = BaggingRegressor(n_estimators=n_trees)
-	models['rf'] = RandomForestRegressor(n_estimators=n_trees)
-	models['et'] = ExtraTreesRegressor(n_estimators=n_trees)
-	models['gbm'] = GradientBoostingRegressor(n_estimators=n_trees)
-	print('Defined %d models' % len(models))
-	return models
- 
-# create a feature preparation pipeline for a model
-def make_pipeline(model):
-	steps = list()
-	# standardization
-	steps.append(('standardize', StandardScaler()))
-	# normalization
-	steps.append(('normalize', MinMaxScaler()))
-	# the model
-	steps.append(('model', model))
-	# create pipeline
-	pipeline = Pipeline(steps=steps)
-	return pipeline
- 
-# evaluate a single model
-def evaluate_model(X, y, model, folds, metric):
-	# create the pipeline
-	pipeline = make_pipeline(model)
-	# evaluate model
-	scores = cross_val_score(pipeline, X, y, scoring=metric, cv=folds, n_jobs=-1)
-	return scores
- 
-# evaluate a model and try to trap errors and hide warnings
-def robust_evaluate_model(X, y, model, folds, metric):
-	scores = None
-	try:
-		with warnings.catch_warnings():
-			warnings.filterwarnings("ignore")
-			scores = evaluate_model(X, y, model, folds, metric)
-	except:
-		scores = None
-	return scores
- 
-# evaluate a dict of models {name:object}, returns {name:score}
-def evaluate_models(X, y, models, folds=10, metric='accuracy'):
-	results = dict()
-	for name, model in models.items():
-		# evaluate the model
-		scores = robust_evaluate_model(X, y, model, folds, metric)
-		# show process
-		if scores is not None:
-			# store a result
-			results[name] = scores
-			mean_score, std_score = mean(scores), std(scores)
-			print(crayons.blue(f'\t[*] NAME => {name}', bold=True))
-			print(crayons.yellow(f'\t[*] Mean Score => {round(mean_score,3)}', bold = True))
-			print(crayons.red(f'\t[*] Std_Score => (+/-){round(std_score,3)}', bold=True))
-			print("\n")
-		else:
-			print('>%s: error' % name)
-	return results
- 
+
+  
 # print and plot the top n results
 def summarize_results(results, maximize=True, top_n=10):
 	# check for no results
@@ -182,9 +69,9 @@ X, y = labels, m
 
 # get model list
 print(crayons.red("\n[*] GETTING THE MODEL LISTS:" , bold=True))
-models = get_models()
+models = regression.get_models()
 # evaluate models
 print(crayons.yellow("\n[*]BEST RESULTING MODELS ACCORDING TO THE DATA...", bold=True))
-results = evaluate_models(X, y, models, metric='mean_squared_error')
+results = regression.evaluate_models(X, y, models, metric='mean_squared_error')
 # summarize results
 summarize_results(results)
