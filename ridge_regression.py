59ff42c1adf232dd67b23d7778570fecd467f9a7
ranand5 <hitanand4@gmail.com>
2018-09-17 15:43:18 +0530

    Adding comment in the ridge_regression.py file

--- ridge_regression.py
+++ ridge_regression.py
@@ -15,34 +15,33 @@ y = data['Close Price']
 data = data.drop(['Close Price'],axis = 1)
 X = pd.DataFrame(data)
 
+#splitting the datasets into train and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
-clf = Ridge(alpha=1.0, normalize=True)
+#calling the model
+clf = Ridge(alpha=0.1, normalize=True)
+#fitting the model with the trainig data
 fit_model = clf.fit(X_train , y_train)
 
+#predicting on the test data
 y_pred = fit_model.predict(X_test)
-#print("Printing the predicted value\n")
 y_pred = pd.Series(y_pred)
-#print(y_pred)
-#print("printing the y_test value \n")
-#print(y_test)
-
 
 y_test1 = y_test.shift(1) #shifting the data one step into the future
 
-y_true = y_test-y_test1
+y_true = y_test-y_test1 
 
 #converting negative data into 0 and postive data into 1.
-y_true[y_true<0] = 0
-y_true[y_true>0] = 1
+y_true[y_true<0] = 0 # '0' stands for down
+y_true[y_true>0] = 1 # '1' stands for up
 
 #changing data into DataFrame
 y_true  = pd.DataFrame(y_true)
+
 #replacing the NAN value with 0.   
 y_true = y_true.fillna(0)
-#changin the data into a numpy array
-#features = np.array(features)
 
+#same applying for predicted value
 y_pred1 = y_pred.shift(1) #shifting the data one step into the future
 
 y_fake = y_pred-y_pred1
@@ -58,11 +57,18 @@ y_fake = y_fake.fillna(0)
 #changin the data into a numpy array
 y_true = np.array(y_true)
 y_fake = np.array(y_fake)
+
+#reshaping the data into 1 dimensional array
 y_true = np.reshape(y_true, len(y_true))
 y_fake = np.reshape(y_fake, len(y_fake))
 
+#comparing corrosponding item in the given two array
 correct_pred = sum(len(set(i)) == 1 for i in zip(y_true, y_fake))
+
+#calculating the accuracy of the model on the basis of up and down
 accuracy = (correct_pred/len(y_true))*100
-print(y_true)
-print(y_fake)
+
+#print(y_true)
+#print(y_fake)
+
 print(accuracy)
