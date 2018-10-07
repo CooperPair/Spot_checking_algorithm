1c5eb72a886ef15a984797d2d6dce95d5ca96f33
ranand5 <hitanand4@gmail.com>
2018-10-07 08:06:02 +0530

    updating file for more readable

--- ridge_algo.py
+++ ridge_algo.py
@@ -1,69 +1,59 @@
-#necessary imports
 import pandas as pd
 import numpy as np
 import sys
 from sklearn.preprocessing import Imputer
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
-#from sklearn.model_selection import KFold
 from matplotlib import pyplot
 import crayons
 
 #loading datasets
-data = pd.read_csv(sys.argv[1])
-data = pd.DataFrame(data)
+raw_data = pd.read_csv(sys.argv[1])
+raw_data = pd.DataFrame(raw_data)
 
-# Finding the right column for the trading
-df = data[['Prev Close','Open Price', 'Last Price']]
+# gettin specific data columns
+training_columns = raw_data[['Prev Close','Open Price', 'Last Price']]
 
-y = data['Close Price']
-print(len(data.columns))
-#data = data.drop(['Close Price'],axis = 1)
+y = raw_data['Close Price']
+X = pd.DataFrame(training_columns)
 
-X = pd.DataFrame(df)
-#print(X)
-#splitting the datasets into train and test set
+#splitting the datasets into train and test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, shuffle=False)
 
-print(len(y_test))
-#calling the model
+#calling model
 clf = Ridge(alpha=0.0, normalize=True, random_state=1)
-#ridge_grid = GridSearchCV(clf, ridge_params, cv=5, verbose=10, scoring='neg_mean_absolute_error') 
-#fitting the model with the trainig data
+#fitting model on training set
 fit_model = clf.fit(X_train , y_train)
 
 #predicting on the test data
 y_pred = fit_model.predict(X_test)
 y_pred = pd.Series(y_pred)
-print(y_pred)
 
-y_test1 = y_test.shift(1) #shifting the data one step into the future
-y_true = y_test-y_test1
+
+y_test_shift = y_test.shift(1) #shifting data one step into the future
+y_true = y_test-y_test_shift
 
 #converting negative data into 0 and postive data into 1.
 y_true[y_true<0] = 0 # '0' stands for down
 y_true[y_true>0] = 1 # '1' stands for up
 
-#changing data into DataFrame
+
 y_true  = pd.DataFrame(y_true)
 
 #replacing the NAN value with 0.   
 y_true = y_true.fillna(0)
-print(y_true)
+
 #same applying for predicted value
-y_pred1 = y_pred.shift(1) #shifting the data one step into the future
+y_pred_shift = y_pred.shift(1) #shifting the data one step into the future
 
-y_fake = y_pred-y_pred1
+y_fake = y_pred-y_pred_shift
 
-#converting negative data into 0 and postive data into 1.
 y_fake[y_fake<0] = 0
 y_fake[y_fake>0] = 1
 
-#changing data into DataFrame
+
 y_fake  = pd.DataFrame(y_fake)
-#replacing the NAN value with 0.   
 y_fake = y_fake.fillna(0)
-#changin the data into a numpy array
 y_true = np.array(y_true)
 y_fake = np.array(y_fake)
 
