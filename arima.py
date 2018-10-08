f4f8932e3a32e0a3e7bb9fb862d0c148811e5d82
ranand5 <hitanand4@gmail.com>
2018-10-08 19:59:18 +0530

    adding the code for the prediction by two models

--- arima.py
+++ arima.py
@@ -4,12 +4,17 @@ from matplotlib import pyplot
 from math import sqrt
 from pandas.tools.plotting import autocorrelation_plot
 from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
 from pandas import DataFrame
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
 import crayons
+from random import random
+
+
+
 
 REPORT = dict()
 
@@ -28,7 +33,7 @@ train, test = X[0:size], X[size:len(X)]
 history = [x for x in train]
 
 predictions = list()
-
+# to do the p,d,q value should be defined for the datasets
 for t in range(len(test)):
         model = ARIMA(history, order=(0,1,1))
         model_fit = model.fit(disp=0)
@@ -74,3 +79,16 @@ for t in range(len(test)):
         obs = test[t]
         history.append(obs)
         i += 1
+
+
+
+# contrived dataset
+sze = len(X)
+train1 = X[0:sze]
+history = [x for x in train1]
+# fit and forecasting model model
+model = ExponentialSmoothing(history, seasonal_periods = 7, seasonal='add',trend = 'add').fit()
+y = model.forecast(steps=1) # to predict one steps into the future
+
+print(y)
+
