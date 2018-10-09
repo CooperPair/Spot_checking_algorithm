d5742d66d4dc7bc724ba5a04eaea249bfc7bce33
ranand5 <hitanand4@gmail.com>
2018-10-09 09:43:11 +0530

    adding the new model for the time series prediction i.e sarimax

--- ar_holt_ensemble.py
+++ ar_holt_ensemble.py
@@ -1,3 +1,5 @@
+# TASK TO DO>> STILL TO FIND THE BEST PARAMERTS ACCORDING TO THE GIVEN PREDICTION
+
 # impoting the modules
 import pandas as pd
 from statsmodels.tsa.arima_model import ARIMA
@@ -6,8 +8,8 @@ from pandas import DataFrame
 from sklearn import model_selection
 import numpy as np
 import sys
+import statsmodels.api as sm
 import crayons
-
 
 # reading the datasets
 data = pd.read_csv(sys.argv[1])
@@ -17,6 +19,7 @@ series = data['Last Price']
 
 # converting it into an array
 X = series.values
+
 # contrived dataset
 sze = len(X)
 
@@ -27,15 +30,18 @@ history = [x for x in train1]
 model1 = ExponentialSmoothing(history, seasonal_periods = 7, seasonal='add',trend = 'add').fit()
 y1 = model1.forecast(steps=1) # to predict one steps into the futur
 
-model2 = ARIMA(history, order=(0,1,1)).fit(disp=-1)
+model2 = ARIMA(history, order=(0,1,1)).fit(disp=0)
 y2 = model2.forecast(steps=1)
+
+model3 = sm.tsa.statespace.SARIMAX(history,order=(1, 1, 1),seasonal_order=(1, 1, 0, 12),enforce_stationarity=False,enforce_invertibility=False).fit()
+y3 = model3.forecast(steps = 1)
 
 print(crayons.yellow(f'\t[*] The prediction from HWM model => {y1[0]}\n', bold = False))
 print(crayons.yellow(f'\t[*] The prediction from ARIMA model => {float(y2[0][0])}\n', bold=False))
-
+print(crayons.yellow(f'\t[*] The prediction from SARIMAX model => {y3[0]}\n', bold = False))
 y1.astype(float)
 y2 = float(y2[0][0])
-
-prediction = pd.Series((y1*0.45 + y2*.55)) 
+y3.astype(float)
+prediction = pd.Series((y1*0.35 + y2*.40 + y3*0.25)) 
 prediction = round(prediction, 2)
 print(crayons.blue(f'\t[*] Total prediction from the ensemble model is  => {prediction[0]}\n', bold=True))
